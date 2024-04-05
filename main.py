5be1e03c53b508ec09e56fb7ddb897c189710b0a
RyanLiut <11930377@mail.sustech.edu.cn>
2024-04-05 12:47:18 +0000

    thresholds from dev

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from nltk.stem import WordNetLemmatizer
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
 def SentInx2TokInx(sent, sentInx):
     blocks = [tokenizer.encode(i, add_special_tokens=False) for i in sent.split()]
@@ -32,26 +32,33 @@ def SentInx2TokInx(sent, sentInx):
     assert [tokenizer.encode(sent)[i] for i in tok_inx] == tokenizer.encode(sent.split()[sentInx], add_special_tokens=False), (sent, sentInx)
     return tok_inx
 
-def extract_sentence(txt_path, gold_path, mark, prev=1):
+def extract_sentence(txt_path, gold_path, mark, prev=0,pos=None):
     df = pd.read_csv(txt_path, header=None, delimiter="\t")
     df.columns = ["target", "pos", "index", "sent1", "sent2"]
     gt = [int(i.strip()=="T") for i in open(gold_path, "r").readlines()]
     df['GT'] = gt
+    if not pos is None:
+        df = df[df["pos"] == pos]
     GT_list = df['GT'].tolist()
     if mark == "base":
         sents_list_1 = df['sent1'].tolist()
         sents_list_2 = df['sent2'].tolist()
         inx_list = df['index']
-    elif mark == "prompt":
+    elif mark == "prompt2":
         target_list = df['target']
-        sents_list_1 = ["The \"%s\" in this sentence:\"%s\" means in one word : \"" % (i,j) for (i,j) in zip(target_list, df['sent1'].tolist())]
-        sents_list_2 = ["The \"%s\" in this sentence:\"%s\" means in one word : \"" % (i,j) for (i,j) in zip(target_list, df['sent2'].tolist())]
+        # sents_list_1 = ["The \"%s\" in this sentence \"%s\" means in one word : \"" % (i,j) for (i,j) in zip(target_list, df['sent1'].tolist())]
+        # sents_list_2 = ["The \"%s\" in this sentence:\"%s\" means in one word : \"" % (i,j) for (i,j) in zip(target_list, df['sent2'].tolist())]
+        sents_list_1 = ["In this sentence \"%s\", \"%s\" means in one word : \"" % (j,i) for (i,j) in zip(target_list, df['sent1'].tolist())]
+        sents_list_2 = ["In this sentence:\"%s\", \"%s\" means in one word : \"" % (j,i) for (i,j) in zip(target_list, df['sent2'].tolist())]
         inx_list = ["%d-%d"%(len(i.split())-1, len(j.split())-1) for i,j in zip(sents_list_1, sents_list_2) ]
     elif mark == "repeat": # TEST later
-        target_list = df['target']
         sents_list_1 = ["%s %s" % (i,i) for i in df['sent1'].tolist()]
         sents_list_2 = ["%s %s" % (i,i) for i in df['sent2'].tolist()]
         inx_list = ["%d-%d"%(len(i.split())+int(inx.split("-")[0])-prev, len(j.split())+int(inx.split("-")[1])-prev) for i,j,inx in zip(df['sent1'], df['sent2'], df['index']) ]
+    elif mark == "repeat_hint":
+        sents_list_1 = ["Rewrite the sentence: %s , rewritten sentence: %s" % (i,i) for i in df['sent1'].tolist()]
+        sents_list_2 = ["Rewrite the sentence: %s , rewritten sentence: %s" % (i,i) for i in df['sent2'].tolist()]
+        inx_list = ["%d-%d"%(len(i.split())+int(inx.split("-")[0])-prev+6, len(j.split())+int(inx.split("-")[1])-prev+6) for i,j,inx in zip(df['sent1'], df['sent2'], df['index']) ]
     return sents_list_1, sents_list_2, GT_list, inx_list
 
 def get_inx_raw(csv_path):
@@ -77,8 +84,13 @@ def extract_sentence_raw(csv_path, mark):
     inx_list = df['inx'].tolist()
 
     if mark == "prompt":
-        sents_list_1 = ["The \"%s\" in this sentence:\"%s\" means in one word : \"" % (i,j) for (i,j) in zip(df['word'], sents_list_1)]
-        sents_list_2 = ["The \"%s\" in this sentence:\"%s\" means in one word : \"" % (i,j) for (i,j) in zip(df['word'], sents_list_2)]
+        sents_list_1 = ["The \"%s\" in this sentence: \"%s\" means in one word : \"" % (i,j) for (i,j) in zip(df['word'], sents_list_1)]
+        sents_list_2 = ["The \"%s\" in this sentence: \"%s\" means in one word : \"" % (i,j) for (i,j) in zip(df['word'], sents_list_2)]
+        inx_list = ["%d-%d"%(len(i.split())-1, len(j.split())-1) for i,j in zip(sents_list_1, sents_list_2) ]
+
+    elif mark == "prompt2":
+        sents_list_1 = ["In this sentence \"%s\", \"%s\" means in one word : \"" % (j,i) for (i,j) in zip(df['word'], sents_list_1)]
+        sents_list_2 = ["In this sentence:\"%s\", \"%s\" means in one word : \"" % (j,i) for (i,j) in zip(df['word'], sents_list_2)]
         inx_list = ["%d-%d"%(len(i.split())-1, len(j.split())-1) for i,j in zip(sents_list_1, sents_list_2) ]
 
     elif mark == "repeat":
@@ -91,7 +103,9 @@ def extract_sentence_raw(csv_path, mark):
 def get_batch_data(sents_list, inx_list, batch_size=50, prompt=None):
     batch_list = []
     tar_inx_list = []
-    for i in range(len(sents_list)//batch_size):
+    for i in range(len(sents_list)//batch_size+1):
+        if i*batch_size == len(sents_list):
+            break
         sent = sents_list[i*batch_size:min(len(sents_list),(i+1)*batch_size)]
         inx = inx_list[i*batch_size:min(len(sents_list),(i+1)*batch_size)]
         tar_inx = [SentInx2TokInx(s,ix) for s,ix in zip(sent, inx)]
@@ -126,18 +140,24 @@ def evaluation(pred, GT, type="acc"):
 def anisotropy_removal(embedding):
     return (embedding - embedding.mean(dim=0)) / (embedding.std(dim=0) + 1e-6)
 
+#%%
 if __name__ == "__main__":
-    root = "YOUR_ROOT_DIR_PATH"
-    txt_path = root+"LLM_LexSem/data/test/test.data.txt"
-    gold_path = root+"LLM_LexSem/data/test/test.gold.txt"
+    root = "/home/liuzhu/"#"YOUR_ROOT_DIR_PATH"
+    mode = "TEST" # or TEST 
+    if mode == "EVAL":
+        txt_path = root+"LLM_LexSem/data/dev/dev.data.txt"
+        gold_path = root+"LLM_LexSem/data/dev/dev.gold.txt"
+    else:
+        txt_path = root+"LLM_LexSem/data/test/test.data.txt"
+        gold_path = root+"LLM_LexSem/data/test/test.gold.txt"
     thresholds = pd.read_csv(root+"LLM_LexSem/thresholds.csv", delimiter=" ") # obtained by a validate set
     raw_path = root+"LLM_LexSem/data/RAW/raw-c_inx.csv"
-    mark = "repeat" 
+    mark = "base"
     tokenizer = LlamaTokenizer.from_pretrained(root+"LLMs/llama-2-7b-hf")
     tokenizer.pad_token = '[PAD]'
 
-    sents_list_1, sents_list_2, GT_list, inx_list = extract_sentence(txt_path, gold_path, mark)
-    # sents_list_1, sents_list_2, GT_list, inx_list = extract_sentence_raw(raw_path, mark)
+    # sents_list_1, sents_list_2, GT_list, inx_list = extract_sentence(txt_path, gold_path, mark, pos="V")
+    sents_list_1, sents_list_2, GT_list, inx_list = extract_sentence_raw(raw_path, mark)
     print(sents_list_1[0], sents_list_2[0], GT_list[0], inx_list[0])
     inx_list_1 = [int(i.split("-")[0]) for i in inx_list]
     inx_list_2 = [int(i.split("-")[1]) for i in inx_list]
@@ -149,27 +169,51 @@ if __name__ == "__main__":
 
     data_pred_layers = {}
     data_sim_layers = {}
-    for batch_1, batch_2, tar_inx_1, tar_inx_2 in tqdm(zip(batch_list_1, batch_list_2, tar_inx_list_1, tar_inx_list_2),total=len(batch_list_1)):
-        batch_rep_1 = get_layer_representations(model, batch_1.to(device), tar_inx_1)
-        batch_rep_2 = get_layer_representations(model, batch_2.to(device), tar_inx_2)
+    for threshold in tqdm(range(0,100,5)):
+        for batch_1, batch_2, tar_inx_1, tar_inx_2 in tqdm(zip(batch_list_1, batch_list_2, tar_inx_list_1, tar_inx_list_2),total=len(batch_list_1)):
+            batch_rep_1 = get_layer_representations(model, batch_1.to(device), tar_inx_1)
+            batch_rep_2 = get_layer_representations(model, batch_2.to(device), tar_inx_2)
 
-        for i in range(1,33):
-            batch_sim = torch.nn.functional.cosine_similarity( anisotropy_removal(batch_rep_1[:,i]), anisotropy_removal(batch_rep_2[:,i]) ).cpu()
-            batch_pred = (batch_sim > thresholds.at[i-1, mark]).int().tolist()
+            for i in range(1,33):
+                batch_sim = torch.nn.functional.cosine_similarity( anisotropy_removal(batch_rep_1[:,i]), anisotropy_removal(batch_rep_2[:,i]) ).cpu()
+                if mode == "TEST":
+                    batch_pred = (batch_sim > thresholds.at[i-1, mark]).int().tolist()
+                    if i in data_pred_layers:
+                        data_pred_layers[i] += batch_pred
+                        data_sim_layers[i] += batch_sim.tolist()
+                    else:
+                        data_pred_layers[i] = batch_pred
+                        data_sim_layers[i] = batch_sim.tolist()
+                else:
+                    batch_pred = (batch_sim > threshold/100).int().tolist()
+                    if threshold not in data_pred_layers:
+                        data_pred_layers[threshold] = {}
+                    if i not in data_pred_layers[threshold]:
+                        data_pred_layers[threshold][i] = []
+                    data_pred_layers[threshold][i] += batch_pred
+        if mode == "TEST":
+            break
 
-            if i in data_pred_layers:
-                data_pred_layers[i] += batch_pred
-                data_sim_layers[i] += batch_sim.tolist()
-            else:
-                data_pred_layers[i] = batch_pred
-                data_sim_layers[i] = batch_sim.tolist()
+    if mode == "TEST":
+        # acc_layers = [evaluation(pred, GT_list) for _, pred in data_pred_layers.items()]
+        # print(acc_layers)
+        # plt.plot(acc_layers)
 
-    acc_layers = [evaluation(pred, GT_list) for _, pred in data_pred_layers.items()]
-    print(acc_layers)
-    plt.plot(acc_layers)
+        corr_layers = [evaluation(sim, GT_list, type="corr") for _, sim in data_sim_layers.items()]
+        print(np.argmax([round(i*100,1) for i in corr_layers]))
+        print([round(i*100,1) for i in corr_layers])
+        print(corr_layers)
+        plt.plot(corr_layers)
+    else:
+        acc_layers = [[evaluation(pred, GT_list) for _, pred in preds.items()] for _, preds in data_pred_layers.items()]
+        acc_layers = np.array(acc_layers)
+        best_th_layers = [list(range(0,100,5))[i]/100 for i in np.argmax(acc_layers, axis=0)]
+        best_acc_layers = np.max(acc_layers, axis=0)
+        plt.plot(best_acc_layers)
+        plt.plot(best_th_layers)
+        for ix,(th,acc) in enumerate(zip(best_th_layers, best_acc_layers)):
+            print("The best accuracy: %.2f\tthreshold: %.2f\tlayer: %d" % (acc,th,ix))
+        thresholds[mark] = best_th_layers
+        thresholds.to_csv(root+"LLM_LexSem/thresholds.csv", sep=" ", index=False)
 
-    # corr_layers = [evaluation(sim, GT_list, type="corr") for _, sim in data_sim_layers.items()]
-    # print(np.argmax([round(i*100,1) for i in corr_layers]))
-    # print([round(i*100,1) for i in corr_layers])
-    # print(corr_layers)
-    # plt.plot(corr_layers)
+# %%
